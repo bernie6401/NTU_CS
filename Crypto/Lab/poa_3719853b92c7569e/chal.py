962e639b12464c0e975d9f1ba2bdf4922282a56a
bernie6401 <bernie6401@gmail.com>
2023-02-17 13:35:31 +0800

    update poa files PS. not complete exploit

--- Crypto/Lab/poa_3719853b92c7569e/chal.py
+++ Crypto/Lab/poa_3719853b92c7569e/chal.py
@@ -2,17 +2,20 @@
 from Crypto.Cipher import AES
 import os
 
-from secret import FLAG
+# from secret import FLAG
+FLAG = b'abcdefg'
+
 
 def pad(data, block_size):
     data += bytes([0x80] + [0x00] * (15 - len(data) % block_size))
-    return data
+    return data # b'abcdefg\x80\x00\x00\x00\x00\x00\x00\x00\x00'
 
 def unpad(data, block_size):
-    if len(data) % block_size:
+    if len(data) % block_size:  # block_size == 16
         raise ValueError
 
     padding_len = 0
+    # Now, data == b'abcdefg\x80\x00\x00\x00\x00\x00\x00\x00\x00'
     for i in range(1, len(data) + 1):
         if data[-i] == 0x80:
             padding_len = i
@@ -22,7 +25,7 @@ def unpad(data, block_size):
     else:
         raise ValueError
 
-    return data[:-padding_len]
+    return data[:-padding_len]  # b'abcdefg'
 
 key = os.urandom(16)
 cipher = AES.new(key, AES.MODE_CBC)
